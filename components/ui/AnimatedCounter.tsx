48e5ef02359ad1b6ce90540c3536e88760b8a503
malak-cs28 <m.mohamed2386@nu.edu.eg>
2025-12-07 09:46:15 +0200

    add doughnut chart using chatjs

--- components/ui/AnimatedCounter.tsx
+++ components/ui/AnimatedCounter.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 import CountUp from 'react-countup'
 const AnimatedCounter = ({amount} : {amount : number}) => {
   return (
-    <div><CountUp end={amount}/></div>
+    <div className='w-full'><CountUp 
+    
+    decimals={2}
+    decimal=","
+    suffix="EGP"  
+    end={amount} /></div>
   )
 }
 
